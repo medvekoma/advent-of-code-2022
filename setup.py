3274ebd261a433db7d31bed2a6f290c8a0aa3ec7
Attila Szucs <attila.szucs@gmail.com>
2022-12-02 15:11:56 +0100

    fix modules

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='advent-of-code-2022', version='1.0', packages=find_packages())
--- utils/Loader.py
+++ utils/Loader.py
@@ -1,4 +1,9 @@
-def load(filename: str = "input.txt") -> list[str]:
+import os
+import inspect
+
+
+def load(filename: str = None) -> list[str]:
+    filename = filename or os.path.dirname((inspect.stack()[1])[1]) + "/input.txt"
     with open(filename) as file:
         return [line.rstrip() for line in file]
 
